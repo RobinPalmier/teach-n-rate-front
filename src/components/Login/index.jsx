a2b05e06640519346f3a129fe00b48bde6381866
RobinPalmier <robinpalmier98@gmail.com>
2020-02-06 20:13:42 +0100

    formulaire connexion

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -4,12 +4,12 @@ import Header from '../Header';
 
 const Login = () => <>
     <h2>Connexion</h2>
-    <Header />
-    <div style={{display:'block'}}>
+    <Header/>
+    <div>
         <form>
-            <input type="email" placeholder="Login" />
-            <input type="password" placeholdeer="password" />
-            <button>Se connecter</button>
+            <input type="email" placeholder="Login" style={{display:'block', width:'20%', minWidth:'182px', maxWidth:'300px', margin: '0 auto', marginTop: '10px'}}/>
+            <input type="password" placeholder="password" style={{display:'block', width:'20%', minWidth:'182px', maxWidth:'300px', margin: '0 auto', marginTop: '10px'}}/>
+            <button style={{display:'block', width:'20%', minWidth:'182px', maxWidth:'300px', margin: '0 auto', marginTop: '10px'}}>Se connecter</button>
         </form>
     </div>
 </>
